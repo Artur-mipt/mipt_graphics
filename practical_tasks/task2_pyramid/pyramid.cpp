dcbebfc58e5f82bd40f8054b8ae776d98d18487f
Artur Mutolapov <amutolapov@MacBook-Pro-Artur-ubic.local>
2021-03-11 01:59:45 +0300

    2nd task done

---
+++ practical_tasks/task2_pyramid/pyramid.cpp
@@ -0,0 +1,204 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+
+#include <common/shader.hpp>
+
+int main( void )
+{
+	if( !glfwInit() )
+	{
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		getchar();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_SAMPLES, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	window = glfwCreateWindow( 1024, 768, "Homework 1 - Colored Pyramid", NULL, NULL);
+	if( window == NULL ){
+		fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = true; // Needed for core profile
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS); 
+
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+
+	GLuint programID = LoadShaders( "TransformVertexShader.vertexshader", "ColorFragmentShader.fragmentshader" );
+	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
+	glm::mat4 View = glm::lookAt(
+        glm::vec3(4,3,-3),
+        glm::vec3(0,0,0),
+        glm::vec3(0,1,0)
+    );
+	glm::mat4 Model = glm::mat4(1.0f);
+	glm::mat4 MVP = Projection * View * Model;
+
+	static const GLfloat g_vertex_buffer_data[] = { 
+        1.0f,0.0f,1.0f,  // 1st triangle
+		-1.0f,0.0f,1.0f,
+		0.0f, 1.0f, 0.0f,
+        1.0f,0.0f,1.0f,  // 2nd triangle
+        -1.0f,0.0f,1.0f,
+        0.0f, -1.0f, 0.0f,
+        0.0f, -1.0f, 0.0f,  // 3rd
+        0.0f, 1.0f, 0.0f,
+        -1.0f,0.0f,1.0f,
+        0.0f, -1.0f, 0.0f,  // 4th
+        0.0f, 1.0f, 0.0f,
+        1.0f,0.0f,1.0f
+	};
+
+	// One color for each vertex. They were generated randomly.
+	static const GLfloat g_color_buffer_data[] = { 
+        0.327f,  0.483f,  0.844f,
+        0.327f,  0.483f,  0.844f,
+		0.327f,  0.483f,  0.844f,
+        0.997f,  0.513f,  0.064f,
+		0.997f,  0.513f,  0.064f,
+        0.997f,  0.513f,  0.064f,
+		0.543f,  0.021f,  0.978f,
+		0.347f,  0.857f,  0.137f,
+        0.822f,  0.569f,  0.501f,
+        0.945f,  0.719f,  0.592f,
+        0.945f,  0.719f,  0.592f,
+        0.945f,  0.719f,  0.592f
+	};
+
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	GLuint colorbuffer;
+	glGenBuffers(1, &colorbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+    
+    float x_cord = 5.0;
+    float y_cord = 5.0;
+    float z_cord = 5.0;
+    float grad = 0.0;
+    int step_counter = 0;
+    
+	do{
+        glm::mat4 View = glm::lookAt(
+            glm::vec3(x_cord, y_cord, z_cord),
+            glm::vec3(0, 0, 0),
+            glm::vec3(0, 1, 0)
+        );
+        
+        grad += 3.14 * 3 / 360;
+        step_counter += 1;
+        int rotate_type = int(step_counter / 200) % 3;
+        if (rotate_type == 0) {
+            x_cord = 5;
+            y_cord = 5 * sin(grad);
+            z_cord = 5 * cos(grad);
+        } else if (rotate_type == 1) {
+            x_cord = 5 * cos(grad);
+            y_cord = 5;
+            z_cord = 5 * sin(grad);
+        } else if (rotate_type == 2) {
+            x_cord = 5 * sin(grad);
+            y_cord = 5 * cos(grad);
+            z_cord = 5;
+        }
+        
+        glm::mat4 MVP = Projection * View * Model;
+
+		// Clear the screen
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		// Use our shader
+		glUseProgram(programID);
+
+		// Send our transformation to the currently bound shader, 
+		// in the "MVP" uniform
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+		glVertexAttribPointer(
+			0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
+		);
+
+		// 2nd attribute buffer : colors
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
+		glVertexAttribPointer(
+			1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
+			3,                                // size
+			GL_FLOAT,                         // type
+			GL_FALSE,                         // normalized?
+			0,                                // stride
+			(void*)0                          // array buffer offset
+		);
+
+		// Draw the triangle !
+		glDrawArrays(GL_TRIANGLES, 0, 4*3); // 12*3 indices starting at 0 -> 12 triangles
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+
+		// Swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} // Check if the ESC key was pressed or the window was closed
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
+
+	// Cleanup VBO and shader
+	glDeleteBuffers(1, &vertexbuffer);
+	glDeleteBuffers(1, &colorbuffer);
+	glDeleteProgram(programID);
+	glDeleteVertexArrays(1, &VertexArrayID);
+
+	// Close OpenGL window and terminate GLFW
+	glfwTerminate();
+
+	return 0;
+}
+
